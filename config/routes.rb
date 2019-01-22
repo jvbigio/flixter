7648ba18adc600cfa7a0cdfe13db9c1926fb9acb
Joe Bigio <jvbigio@gmail.com>
2019-01-22 21:21:43 +0000

    Working on instructor dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  resources :courses, only: [:index, :show] do 
+  resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do
-      resources :lessons, only: [:create]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:create] 
+      resources :sections, only: [:new, :create]
     end
   end
 end
