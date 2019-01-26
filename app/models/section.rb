e2b6c48f663b5e1aaf4327b9e2d88dc8808bd67d
Joe Bigio <jvbigio@gmail.com>
2019-01-26 15:01:26 +0000

    Fixed syntax error in section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,5 +7,6 @@ class Section < ApplicationRecord
 
   def next_section
     section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-    return section 
+    return section
+  end 
 end
